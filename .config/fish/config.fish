27f6ad35e30c57b1ebb60af7447c6019fc2ec797
yasuyuki toriumi <toriumi0118@gmail.com>
2018-02-20 20:55:34 +0900

    fix: anyenv for pyenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,28 @@ alias tmux='tmux -u'
 
 # use anyenv
 set -x PATH $HOME/.anyenv/bin $PATH
+# eval (anyenv init -)
+
+# pyenv 
+set -x PYENV_ROOT "$HOME/.anyenv/envs/pyenv"
+set -x PATH $PATH "$HOME/.anyenv/envs/pyenv/bin"
+# [pyenv init - fish]
+set -gx PATH "$HOME/.anyenv/envs/pyenv/shims" $PATH
+set -gx PYENV_SHELL fish
+source "$HOME/.anyenv/envs/pyenv/libexec/../completions/pyenv.fish"
+command pyenv rehash 2>/dev/null
+function pyenv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case rehash shell
+    source (pyenv "sh-$command" $argv|psub)
+  case '*'
+    command pyenv "$command" $argv
+  end
+end
+####################
 
 ### Add Command for peco with z
 function peco_z

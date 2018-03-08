48fd21a0ce781b9a41cead7cb13af71bbb298bda
Yasuyuki Toriumi <toriumi0118@gmail.com>
2018-03-08 18:01:37 +0900

    fix: separate install1,2.sh

---
+++ install1.sh
@@ -0,0 +1,37 @@
+# !/bin/bash
+echo '>>>>> START install1.sh'
+
+# install fish
+echo ''
+echo '>>>>>>> INSTALL fish'
+brew install fish reattach-to-user-namespace
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+
+# install tmux
+echo ''
+echo '>>>>>>> INSTALL tmux'
+brew install tmux
+
+# copy setting files
+echo ''
+echo '>>>>>>> COPY setting files'
+cp -R .ssh ~/
+cp -R .config ~/
+cp -R .vim ~/
+cp .tmux.conf ~/
+cp .vimrc ~/
+
+# install vim plugins
+echo ''
+echo '>>>>>>> INSTALL vim-plug'
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo '>>>>>>> SWITCH default shell'
+echo '>>>>>>>>> ADD `/usr/local/bin/fish` to /etc/shells'
+echo 'vim /etc/shells'
+echo '>>>>>>>>> CHANGE DEFAULT SHELL'
+echo 'chsh -s /usr/local/bin/fish'
+
+echo ''
+echo '>>>>> FINISH install1.sh'
---
+++ install2.sh
@@ -0,0 +1,15 @@
+# !/usr/local/bin/fish
+echo '>>>>> START install2.sh'
+
+# install file utils
+echo ''
+echo '>>>>>>> INSTALL fish utils'
+brew install peco
+fisher z fzf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.2
+echo 'source ~/.asdf/asdf.fish' >> ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/completions; and cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+brew install coreutils automake autoconf openssl libyaml readline libxslt libtool unixodbc
+
+echo ''
+echo '>>>>> FINISH install2.sh'

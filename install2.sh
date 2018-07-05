3689c54036a153753e0a7e2eb688e57b9f168caf
toriumi0118 <toriumi0118@gmail.com>
2018-07-05 21:42:34 +0900

    add: tree

--- install2.sh
+++ install2.sh
@@ -4,7 +4,7 @@ echo '>>>>> START install2.sh'
 # install file utils
 echo ''
 echo '>>>>>>> INSTALL fish utils'
-brew install peco ghq
+brew install peco ghq tree
 fisher z fzf oh-my-fish/plugin-balias
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.2
 echo 'source ~/.asdf/asdf.fish' >> ~/.config/fish/config.fish

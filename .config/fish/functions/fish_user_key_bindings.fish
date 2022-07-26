f0d3d4ec3a2d83b45c9ae2906908624714bc4f5b
Yasuyuki Toriumi <toriumi0118@gmail.com>
2022-07-26 10:53:16 +0900

    fix: keybindを変更

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,30 +1,18 @@
 function fish_user_key_bindings
     ### peco_z ###
     bind \c] peco_z # Ctrl-]にバインドする
+    bind -M insert \c] peco_z # Ctrl-]にバインドする
     ### peco_z ###
     ### fzf ###
-    if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
-        bind \ct '__fzf_find_file'
-        bind \cr '__fzf_reverse_isearch'
-        bind \ec '__fzf_cd'
-        bind \eC '__fzf_cd_with_hidden'
-        if bind -M insert >/dev/null 2>/dev/null
-            bind -M insert \ct '__fzf_find_file'
-            bind -M insert \cr '__fzf_reverse_isearch'
-            bind -M insert \ec '__fzf_cd'
-            bind -M insert \eC '__fzf_cd --hidden'
-        end
-    else
-        bind \cf '__fzf_find_file'
-        bind \cr '__fzf_reverse_isearch'
-        bind \eo '__fzf_cd'
-        bind \eO '__fzf_cd --hidden'
-        if bind -M insert >/dev/null 2>/dev/null
-            bind -M insert \cf '__fzf_find_file'
-            bind -M insert \cr '__fzf_reverse_isearch'
-            bind -M insert \eo '__fzf_cd'
-            bind -M insert \eO '__fzf_cd --hidden'
-        end
+    bind \cf '__fzf_find_file'
+    bind \cr '__fzf_reverse_isearch'
+    bind \co '__fzf_cd'
+    bind \ch '__fzf_cd --hidden'
+    if bind -M insert >/dev/null 2>/dev/null
+        bind -M insert \cf '__fzf_find_file'
+        bind -M insert \cr '__fzf_reverse_isearch'
+        bind -M insert \co '__fzf_cd'
+        bind -M insert \ch '__fzf_cd --hidden'
     end
     if set -q FZF_COMPLETE
         bind \t '__fzf_complete'
@@ -32,6 +20,7 @@ function fish_user_key_bindings
     ### fzf ###
 
     fish_vi_key_bindings insert
+    bind -M insert \ce accept-autosuggestion
 end
 
 fzf_key_bindings

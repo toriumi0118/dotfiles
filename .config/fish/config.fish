d10ae87db4eaf3565f767f4b0fc0fc0182a3b4fa
Yasuyuki Toriumi <toriumi0118@YasuyukinoMac-mini.local>
2023-02-09 02:54:37 +0900

    fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,28 +1,31 @@
-### Aliases
-# For MacVim (ctagsとVimを優先するため前置)
-# set -gx PATH '/Applications/MacVim.app/Contents/bin' $PATH
-# alias vi='/Applications/MacVim.app/Contents/bin/vim'
-. ~/.config/fish/aliases.fish
-alias vim='nvim'
-alias tmux='tmux -u'
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    . ~/.config/fish/aliases.fish
+    alias vim='nvim'
+    alias tmux='tmux -u'
 
-### Add Command for peco with z
-function peco_z
-  set -l query (commandline)
-
-  if test -n $query
-    set peco_flags --query "$query"
-  end
-
-  z -l | peco $peco_flags | awk '{ print $2 }' | read recent
-  if [ $recent ]
-    cd $recent
-    commandline -r ''
-    commandline -f repaint
+    ### Add Command for peco with z
+    function peco_z
+      set -l query (commandline)
+    
+      if test -n $query
+        set peco_flags --query "$query"
+      end
+    
+      z -l | peco $peco_flags | awk '{ print $2 }' | read recent
+      if [ $recent ]
+        cd $recent
+        commandline -r ''
+        commandline -f repaint
+        end
     end
-end
-source ~/.asdf/asdf.fish
+    
+    ### Default Environments
+    set --export EDITOR nvim
+    set --export PATH $PATH /Users/toriumi0118/.bin # terrafrom-lsp
 
-### Default Environments
-set --export EDITOR nvim
-set --export PATH $PATH /Users/toriumi0118/.bin # terrafrom-lsp
+    # Set PATH, MANPATH, etc., for Homebrew.
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -22,3 +22,5 @@ function fish_user_key_bindings
     fish_vi_key_bindings insert
     bind -M insert \ce accept-autosuggestion
 end
+
+fzf_key_bindings

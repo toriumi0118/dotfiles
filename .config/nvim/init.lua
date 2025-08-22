a2183bf02dfb10b8e253aede115b1f7bb9bdf51a
toriumi0118 <toriumi0118@gmail.com>
2025-08-22 12:04:45 +0900

    fix: rm nvim

--- .config/nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require("config.core")
-require("config.lazy")
-require("config.keymaps")
--- .config/nvim/lua/plugins/easy-align.lua
+++
@@ -1,9 +0,0 @@
-local M = {
-	"junegunn/vim-easy-align",
-	lazy = true,
-	keys = {
-		{ "ga", "<Plug>(EasyAlign)", mode = { "n", "x" }, desc = "Easy Align" },
-	},
-}
-
-return { M }

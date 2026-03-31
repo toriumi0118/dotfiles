158bbf3f0bb484fe7a87fc39cecf9b585504ecf2
toriumi0118 <toriumi0118@gmail.com>
2026-04-01 02:00:06 +0900

    fix: !

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,4 +19,24 @@ end
 
 zoxide init fish | source
 
+set _asdf_shims "$HOME/.asdf/shims"
+set -gx --prepend PATH $_asdf_shims
+set --erase _asdf_shims
+
 set --export ANTHROPIC_API_KEY $(security find-generic-password -s "ANTHROPIC_API_KEY" -w)
+~/.local/bin/mise activate fish | source
+
+# pnpm
+set -gx PNPM_HOME /Users/toriumi0118/Library/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# wtp 
+wtp shell-init fish | source
+
+# direnv
+direnv hook fish | source
+
+source ~/.safe-chain/scripts/init-fish.fish # Safe-chain Fish initialization script
